0c5e30a52fbf8d0a9c458ebb57c39cfcadaa92bc
NareshPatilOfficial <nareshpatil.nrp@gmail.com>
2023-04-01 16:11:30 +0530

    Used LinearGradient and ImageBackground component for background

--- App.js
+++ App.js
@@ -1,18 +1,29 @@
-import { View, StyleSheet } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import { StyleSheet, ImageBackground } from "react-native";
 import StartGameScreen from "./src/screens/StartGameScreen";
+import BackGroundImage from "./assets/images/backGround.png";
 
 export default function App() {
   console.log('Started Project');
   return (
-    <View style={styles.rootContainer}>
-      <StartGameScreen />
-    </View>
+    <LinearGradient colors={['#4e0329', '#ddb52f']} style={styles.rootContainer}>
+      <ImageBackground 
+      source={BackGroundImage}
+      resizeMode="cover"
+      style={styles.rootContainer}
+      imageStyle={styles.backGroungImage}
+      >
+        <StartGameScreen />
+      </ImageBackground>
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
   rootContainer:{
-    backgroundColor:'#ddb52f',
     flex:1
+  },
+  backGroungImage:{
+    opacity:0.15
   }
 })
